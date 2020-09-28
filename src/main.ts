4e988908aee090824f64bb8fa451412e0e459221
Alice Su <toalice@protonmail.com>
2020-09-28 13:09:01 -0700

    test
    Signed-off-by: Alice Su <toalice@protonmail.com>

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function run(): Promise<void> {
     const branch: string = getInput('branch');
     debug(`Will write to branch ${branch} `); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
-    setFailed(error.message);
+    // setFailed(error.message);
   }
 }
 
