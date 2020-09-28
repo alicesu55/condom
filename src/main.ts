66aed559ba54806d76df49a932be04f373578147
Alice Su <toalice@protonmail.com>
2020-09-27 19:50:50 -0700

    bug fix
    Signed-off-by: Alice Su <toalice@protonmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,9 @@ import {exec} from 'child_process';
 
 async function run(): Promise<void> {
   try {
-    exec('ls -lh', (error, stdout, stderr) => console.log(stdout));
-    const ms: string = getInput('milliseconds');
-    debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    debug(new Date().toTimeString());
-
-    setOutput('time', new Date().toTimeString());
+    exec('ls -lh', (error, stdout, stderr) => debug(stdout));
+    const branch: string = getInput('branch');
+    debug(`Will write to branch ${branch} `); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
     setFailed(error.message);
   }
