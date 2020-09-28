2f2c037475114bb8cea80e9996f9cab06835dfc1
Alice Su <toalice@protonmail.com>
2020-09-28 11:26:26 -0700

    copy to __condom__ directory
    Signed-off-by: Alice Su <toalice@protonmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import {exec} from 'child_process';
 
 async function run(): Promise<void> {
   try {
+    exec(
+      "mkdir __condom__; tar -cf -  --exclude '__condom__' . | tar -xC __condom__",
+      (error, stdout, stderr) => debug(stdout)
+    );
     exec('ls -lh', (error, stdout, stderr) => debug(stdout));
+    exec('ls -lh __condom__', (error, stdout, stderr) => debug(stdout));
     const branch: string = getInput('branch');
     debug(`Will write to branch ${branch} `); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
